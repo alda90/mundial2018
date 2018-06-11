54f93d7940d54c4c9a7b53e378cfaef37a97c085
Aldair Carrillo <aldacdh904@gmail.com>
2018-06-10 22:29:43 -0600

    make it better 2

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,2 +1,2 @@
-//export const URL_SERVICIOS = 'https://mundial-backend.herokuapp.com';
-export const URL_SERVICIOS = 'http://localhost:3000';
+export const URL_SERVICIOS = 'https://mundial-backend.herokuapp.com';
+//export const URL_SERVICIOS = 'http://localhost:3000';
